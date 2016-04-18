7b19295bba98db7a070f06170afcc26202b9dfb1
manuel.hegner <manuel.hegner@bakdata.com>
2016-04-18 14:21:39 +0200

    added iterator for fields

--- clients/scala/src/main/java/com/marmar/GridUtil.java
+++ clients/scala/src/main/java/com/marmar/GridUtil.java
@@ -6,8 +6,23 @@ import java.util.List;
 import tyckiting.Position;
 
 public class GridUtil {
-	public List<Position> iterateRadius(int x, int y, int r) {
+	public static List<Position> iterateRadius(int x, int y, int r) {
 		List<Position> l=new ArrayList<>();
-		
+		for(int i=0;i<=r;i++) {
+			for(int sx=-i,sy=i-r;sx<=r;sx++) {
+				l.add(new Position(x+sx,y+sy));
+			}
+			
+			if(i!=0) {
+				for(int sx=-r,sy=i;sx<=r-i;sx++) {
+					l.add(new Position(x+sx,y+sy));
+				}
+			}
+		}
+		return l;
+	}
+	
+	public static void main(String[] args) {
+		iterateRadius(0, 0, 2);
 	}
 }
